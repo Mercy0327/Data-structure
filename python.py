51fe0f869dff827fb08998d440d7cd3485752681
Mercy0327 <mercy.phiri.mp@gmail.com>
2025-07-06 17:08:34 +0200

    data structure complete

---
+++ python.py
@@ -0,0 +1,27 @@
+#Operators
+
+# Addition (+)
+# Subtraction (-)
+# Multiplication (*)
+# Division (/)
+# Modulus (%)
+# Exponent (**)
+
+x = 10
+y = 2
+
+print(x + y)
+print(x-y)
+print(x* y)
+print(x -y)
+print (x/ y)
+print (x % y)
+print(5%2)
+
+print(x **y)
+
+x = 10
+# x = x + 2
+x -= 2
+
+print(x)
